4165d35e561b652847740824ae696b74a6471b85
Tomasz Bielski <ttbielski@gmail.com>
2022-09-09 20:44:07 +0200

    Add solution

---
+++ 8-kyu/0007-return-negative.js
@@ -0,0 +1,7 @@
+// Link: https://www.codewars.com/kata/55685cd7ad70877c23000102/train/javascript
+
+// SOLUTION ///////////////////////////////////////////////////////////////////
+
+function makeNegative(num) {
+  return -Math.abs(num);
+}
