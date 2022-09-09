5af7fcb09a0150caf919930e78c129ca50d19d3b
Tomasz Bielski <ttbielski@gmail.com>
2022-09-09 02:00:17 +0200

    Add solution

---
+++ 8-kyu/multiply.js
@@ -0,0 +1,7 @@
+// Link: https://www.codewars.com/kata/50654ddff44f800200000004/train/javascript
+
+// SOLUTION ///////////////////////////////////////////////////////////////////
+
+function multiply(a, b){
+  return a * b;
+}
