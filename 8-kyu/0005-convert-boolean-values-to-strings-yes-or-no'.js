d34a7d5d31efbca1c967546a4da5462886a814dd
Tomasz Bielski <ttbielski@gmail.com>
2022-09-09 17:34:21 +0200

    Add solution

---
+++ 8-kyu/0005-convert-boolean-values-to-strings-yes-or-no'.js
@@ -0,0 +1,7 @@
+// Link: https://www.codewars.com/kata/53369039d7ab3ac506000467/train/javascript
+
+// SOLUTION ///////////////////////////////////////////////////////////////////
+
+function boolToWord(bool){
+  return bool ? 'Yes' : 'No';
+}
