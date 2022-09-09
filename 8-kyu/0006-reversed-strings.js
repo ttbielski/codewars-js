3f525c488d6f3210394b4b48738397615a41e119
Tomasz Bielski <ttbielski@gmail.com>
2022-09-09 17:36:55 +0200

    Add solution

---
+++ 8-kyu/0006-reversed-strings.js
@@ -0,0 +1,7 @@
+// Link: https://www.codewars.com/kata/5168bb5dfe9a00b126000018/train/javascript
+
+// SOLUTION ///////////////////////////////////////////////////////////////////
+
+function solution(str){
+  return str.split('').reverse().join('');
+}
