e42eb6b1892068ecd2e3644401272083fd895a8c
Tomasz Bielski <ttbielski@gmail.com>
2022-09-09 02:08:28 +0200

    Add solution

---
+++ 8-kyu/even-or-odd.js
@@ -0,0 +1,7 @@
+// Link: https://www.codewars.com/kata/53da3dbb4a5168369a0000fe/train/javascript
+
+// SOLUTION ///////////////////////////////////////////////////////////////////
+
+function evenOrOdd(number) {
+  return number % 2 === 0 ? 'Even' : 'Odd';
+}
