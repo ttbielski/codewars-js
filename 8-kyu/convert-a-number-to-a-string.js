5982de2287f01531ac903c4c29faaa7489151178
Tomasz Bielski <ttbielski@gmail.com>
2022-09-09 02:14:43 +0200

    Add solution

---
+++ 8-kyu/convert-a-number-to-a-string.js
@@ -0,0 +1,7 @@
+// Link: https://www.codewars.com/kata/5265326f5fda8eb1160004c8/train/javascript
+
+// SOLUTION ///////////////////////////////////////////////////////////////////
+
+function numberToString(num) {
+  return String(num);
+}
