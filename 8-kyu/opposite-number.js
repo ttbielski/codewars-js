211d460d1c0faff6b382ee013b65c44e5e3f3e6b
Tomasz Bielski <ttbielski@gmail.com>
2022-09-09 02:11:37 +0200

    Add solution

---
+++ 8-kyu/opposite-number.js
@@ -0,0 +1,7 @@
+// Link: https://www.codewars.com/kata/56dec885c54a926dcd001095/train/javascript
+
+// SOLUTION ///////////////////////////////////////////////////////////////////
+
+function opposite(number) {
+  return -number;
+}
