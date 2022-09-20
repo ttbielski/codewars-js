d3441f8cfaac6486011b90e5e512189e857af84a
Tomasz Bielski <ttbielski@gmail.com>
2022-09-20 19:52:42 +0200

    Add solution

---
+++ 8-kyu/0067-count-the-monkeys.js
@@ -0,0 +1,13 @@
+// Link: https://www.codewars.com/kata/56f69d9f9400f508fb000ba7/train/javascript
+
+// SOLUTION ///////////////////////////////////////////////////////////////////
+
+function monkeyCount(n) {
+  const monkeys = [];
+  
+  for (let i = 1; i <= n; i++) {
+    monkeys.push(i);
+  }
+  
+  return monkeys;
+}
