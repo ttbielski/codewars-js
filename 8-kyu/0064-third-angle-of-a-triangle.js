6a41163827c070a611e05eddf86ac0b667844e0c
Tomasz Bielski <ttbielski@gmail.com>
2022-09-20 19:42:00 +0200

    Add solution

---
+++ 8-kyu/0064-third-angle-of-a-triangle.js
@@ -0,0 +1,7 @@
+// Link: https://www.codewars.com/kata/5a023c426975981341000014/train/javascript
+
+// SOLUTION ///////////////////////////////////////////////////////////////////
+
+function otherAngle(a, b) {
+  return 180 - (a + b);
+}
