430a82be349147e82632c60300c40c87a6461f91
Tomasz Bielski <ttbielski@gmail.com>
2022-09-20 19:45:24 +0200

    Add solution

---
+++ 8-kyu/0065-l1-set-alarm.js
@@ -0,0 +1,7 @@
+// Link: https://www.codewars.com/kata/568dcc3c7f12767a62000038/train/javascript
+
+// SOLUTION ///////////////////////////////////////////////////////////////////
+
+function setAlarm(employed, vacation){
+  return employed && !vacation;
+}
