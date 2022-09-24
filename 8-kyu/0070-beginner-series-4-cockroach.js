d174a05dee50b10129797cab7d27d4d0c9f5650d
Tomasz Bielski <ttbielski@gmail.com>
2022-09-24 17:16:23 +0200

    Add solution

---
+++ 8-kyu/0070-beginner-series-4-cockroach.js
@@ -0,0 +1,7 @@
+// Link: https://www.codewars.com/kata/55fab1ffda3e2e44f00000c6/train/javascript
+
+// SOLUTION ///////////////////////////////////////////////////////////////////
+
+function cockroachSpeed(s) {
+  return Math.floor(s * 27.778);
+}
