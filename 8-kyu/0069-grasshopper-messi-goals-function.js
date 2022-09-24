bb76fd073feb28a1381346bcff568332844d19cc
Tomasz Bielski <ttbielski@gmail.com>
2022-09-24 17:13:01 +0200

    Add solution

---
+++ 8-kyu/0069-grasshopper-messi-goals-function.js
@@ -0,0 +1,7 @@
+// Link: https://www.codewars.com/kata/55f73be6e12baaa5900000d4/train/javascript
+
+// SOLUTION ///////////////////////////////////////////////////////////////////
+
+function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+  return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+}
