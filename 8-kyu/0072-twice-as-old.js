314782d233a107afd6d713b4b53bb8a7beed105e
Tomasz Bielski <ttbielski@gmail.com>
2022-09-28 13:51:33 +0200

    Add solution

---
+++ 8-kyu/0072-twice-as-old.js
@@ -0,0 +1,7 @@
+// Link: https://www.codewars.com/kata/5b853229cfde412a470000d0/train/javascript
+
+// SOLUTION ///////////////////////////////////////////////////////////////////
+
+function twiceAsOld(dadYearsOld, sonYearsOld) {
+  return Math.abs(dadYearsOld - sonYearsOld * 2);
+}
