2a2e4795688e507c947cbbab3832df949582661f
Tomasz Bielski <ttbielski@gmail.com>
2022-09-28 13:55:42 +0200

    Add solution

---
+++ 8-kyu/0073-parse-nice-int-from-char-problem.js
@@ -0,0 +1,7 @@
+// Link: https://www.codewars.com/kata/557cd6882bfa3c8a9f0000c1/train/javascript
+
+// SOLUTION ///////////////////////////////////////////////////////////////////
+
+function getAge(inputString) {
+  return Number(inputString[0]);
+}
