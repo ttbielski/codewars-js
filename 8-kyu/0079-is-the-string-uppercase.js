6d72dcb14c09ba49996f7aad567d435e6c98a2b9
Tomasz Bielski <ttbielski@gmail.com>
2022-09-29 18:54:26 +0200

    Add solution

---
+++ 8-kyu/0079-is-the-string-uppercase.js
@@ -0,0 +1,7 @@
+// Link: https://www.codewars.com/kata/56cd44e1aa4ac7879200010b/train/javascript
+
+// SOLUTION ///////////////////////////////////////////////////////////////////
+
+String.prototype.isUpperCase = function() {
+  return this.toString() === this.toUpperCase();
+}
