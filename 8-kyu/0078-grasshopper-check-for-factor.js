c0e623eb1ecaab682ddc4eaaa9c9f9114ff6224f
Tomasz Bielski <ttbielski@gmail.com>
2022-09-29 18:52:28 +0200

    Add solution

---
+++ 8-kyu/0078-grasshopper-check-for-factor.js
@@ -0,0 +1,7 @@
+// Link: https://www.codewars.com/kata/55cbc3586671f6aa070000fb/train/javascript
+
+// SOLUTION ///////////////////////////////////////////////////////////////////
+
+function checkForFactor (base, factor) {
+  return base % factor === 0;
+}
