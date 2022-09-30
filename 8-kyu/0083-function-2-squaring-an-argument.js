f81fb84821f21e660c76b153d01dcbfd3e068f03
Tomasz Bielski <ttbielski@gmail.com>
2022-09-30 14:54:08 +0200

    Add solution

---
+++ 8-kyu/0083-function-2-squaring-an-argument.js
@@ -0,0 +1,7 @@
+// Link: https://www.codewars.com/kata/523b623152af8a30c6000027/train/javascript
+
+// SOLUTION ///////////////////////////////////////////////////////////////////
+
+function square(n) {
+  return n * n;
+}
