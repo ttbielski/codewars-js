012b4c2bfa7a938eabdfb1480bc50a7755d2c98e
Tomasz Bielski <ttbielski@gmail.com>
2022-10-01 12:41:43 +0200

    Add solution

---
+++ 8-kyu/0085-grasshopper-terminal-game-move-function.js
@@ -0,0 +1,7 @@
+// Link: https://www.codewars.com/kata/563a631f7cbbc236cf0000c2/train/javascript
+
+// SOLUTION ///////////////////////////////////////////////////////////////////
+
+function move (position, roll) {
+  return position + roll * 2;
+}
