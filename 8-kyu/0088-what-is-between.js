94723de07638ae90556b84e935353f419ef64cc4
Tomasz Bielski <ttbielski@gmail.com>
2022-10-01 12:50:04 +0200

    Add solution

---
+++ 8-kyu/0088-what-is-between.js
@@ -0,0 +1,13 @@
+// Link: https://www.codewars.com/kata/55ecd718f46fba02e5000029/train/javascript
+
+// SOLUTION ///////////////////////////////////////////////////////////////////
+
+function between(a, b) {
+  const arr = [];
+  
+  for (let i = a; i <= b; i++) {
+    arr.push(i);
+  }
+  
+  return arr;
+}
