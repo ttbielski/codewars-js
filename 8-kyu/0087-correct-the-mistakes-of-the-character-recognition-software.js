1078e1f18dfade704c7d39ab74701514d88c8347
Tomasz Bielski <ttbielski@gmail.com>
2022-10-01 12:48:03 +0200

    Add solution

---
+++ 8-kyu/0087-correct-the-mistakes-of-the-character-recognition-software.js
@@ -0,0 +1,7 @@
+// Link: https://www.codewars.com/kata/577bd026df78c19bca0002c0/train/javascript
+
+// SOLUTION ///////////////////////////////////////////////////////////////////
+
+function correct(string) {
+  return string.replace(/5/gi, 'S').replace(/0/gi, 'O').replace(/1/gi, 'I');
+}
