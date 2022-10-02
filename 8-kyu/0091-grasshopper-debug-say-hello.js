f21c9a6dbfa54595a3c49fe85ab4ffb9790081d6
Tomasz Bielski <ttbielski@gmail.com>
2022-10-02 14:10:24 +0200

    Add solution

---
+++ 8-kyu/0091-grasshopper-debug-say-hello.js
@@ -0,0 +1,7 @@
+// Link: https://www.codewars.com/kata/5625618b1fe21ab49f00001f/train/javascript
+
+// SOLUTION ///////////////////////////////////////////////////////////////////
+
+function sayHello(name) {
+  return `Hello, ${name}`;
+}
