25e8a567e0705d5f422cdd4f58758bb39d4d582d
Tomasz Bielski <ttbielski@gmail.com>
2022-10-02 14:05:36 +0200

    Add solution

---
+++ 8-kyu/0089-is-it-even.js
@@ -0,0 +1,7 @@
+// Link: https://www.codewars.com/kata/555a67db74814aa4ee0001b5/train/javascript
+
+// SOLUTION ///////////////////////////////////////////////////////////////////
+
+function testEven(n) {
+  return n % 2 === 0;
+}
