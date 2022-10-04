5b4456f8e9e11cc6f170e4262212ea2f64d0b7eb
Tomasz Bielski <ttbielski@gmail.com>
2022-10-04 20:31:40 +0200

    Add solution

---
+++ 8-kyu/0094-welcome.js
@@ -0,0 +1,27 @@
+// Link: https://www.codewars.com/kata/577ff15ad648a14b780000e7/train/javascript
+
+// SOLUTION ///////////////////////////////////////////////////////////////////
+
+function greet(language) {
+	const languages = {
+    english: 'Welcome',
+    czech: 'Vitejte',
+    danish: 'Velkomst',
+    dutch: 'Welkom',
+    estonian: 'Tere tulemast',
+    finnish: 'Tervetuloa',
+    flemish: 'Welgekomen',
+    french: 'Bienvenue',
+    german: 'Willkommen',
+    irish: 'Failte',
+    italian: 'Benvenuto',
+    latvian: 'Gaidits',
+    lithuanian: 'Laukiamas',
+    polish: 'Witamy',
+    spanish: 'Bienvenido',
+    swedish: 'Valkommen',
+    welsh: 'Croeso'
+  };
+  
+  return languages[language] || languages['english'];
+}
