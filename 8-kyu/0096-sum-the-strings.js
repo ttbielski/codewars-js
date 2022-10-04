6428c991c800203ee29bdeb7c96ac4b994969fe5
Tomasz Bielski <ttbielski@gmail.com>
2022-10-04 20:37:45 +0200

    Add solution

---
+++ 8-kyu/0096-sum-the-strings.js
@@ -0,0 +1,7 @@
+// Link: https://www.codewars.com/kata/5966e33c4e686b508700002d/train/javascript
+
+// SOLUTION ///////////////////////////////////////////////////////////////////
+
+function sumStr(a, b) {
+  return String(Number(a) + Number(b));
+}
