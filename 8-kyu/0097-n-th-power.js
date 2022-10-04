feb7fd522f6c4c6e2a528166fa93d1d5144fff76
Tomasz Bielski <ttbielski@gmail.com>
2022-10-04 20:43:40 +0200

    Add solution

---
+++ 8-kyu/0097-n-th-power.js
@@ -0,0 +1,7 @@
+// Link: https://www.codewars.com/kata/57d814e4950d8489720008db/train/javascript
+
+// SOLUTION ///////////////////////////////////////////////////////////////////
+
+function index(array, n){
+  return array[n] ? array[n] ** n : -1;
+}
