18c7d324ebe355a7b61be09055ea87554d71ed61
Tomasz Bielski <ttbielski@gmail.com>
2022-10-05 15:37:04 +0200

    Add solution

---
+++ 8-kyu/0098-sort-and-star.js
@@ -0,0 +1,7 @@
+// Link: https://www.codewars.com/kata/57cfdf34902f6ba3d300001e/train/javascript
+
+// SOLUTION ///////////////////////////////////////////////////////////////////
+
+function twoSort(s) {
+  return s.sort()[0].split('').join('***');
+}
