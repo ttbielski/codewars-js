25cdb89828689b12bfc2339060556e63818999bc
Tomasz Bielski <ttbielski@gmail.com>
2022-10-07 19:10:22 +0200

    Add solution

---
+++ 8-kyu/0103-regular-ball-super-ball.js
@@ -0,0 +1,7 @@
+// Link: https://www.codewars.com/kata/53f0f358b9cb376eca001079/train/javascript
+
+// SOLUTION ///////////////////////////////////////////////////////////////////
+
+function Ball(ballType = 'regular') {
+  this.ballType = ballType;
+}
