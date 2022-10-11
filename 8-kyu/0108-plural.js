a306d36c803bff1a88859db48197d4d0c907804f
Tomasz Bielski <ttbielski@gmail.com>
2022-10-11 18:01:33 +0200

    Add solution

---
+++ 8-kyu/0108-plural.js
@@ -0,0 +1,7 @@
+// Link: https://www.codewars.com/kata/52ceafd1f235ce81aa00073a/train/javascript
+
+// SOLUTION ///////////////////////////////////////////////////////////////////
+
+function plural(n) {
+  return n !== 1;
+}
