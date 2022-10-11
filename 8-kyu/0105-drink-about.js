4b87d519d06ddd3da74d7c4a1e8e67e8917d52f4
Tomasz Bielski <ttbielski@gmail.com>
2022-10-11 17:51:01 +0200

    Add solution

---
+++ 8-kyu/0105-drink-about.js
@@ -0,0 +1,15 @@
+// Link: https://www.codewars.com/kata/56170e844da7c6f647000063/train/javascript
+
+// SOLUTION ///////////////////////////////////////////////////////////////////
+
+function peopleWithAgeDrink(old) {
+  if (old < 14) {
+    return 'drink toddy';
+  } else if (old < 18) {
+    return 'drink coke';
+  } else if (old < 21) {
+    return 'drink beer';
+  } else {
+    return 'drink whisky';
+  }
+}
