b40702b8d43fc4bd9af76187921e7cb9a3b74cd9
Tomasz Bielski <ttbielski@gmail.com>
2022-10-11 18:03:16 +0200

    Add solution

---
+++ 8-kyu/0109-how-many-lightsabers-do-you-own.js
@@ -0,0 +1,7 @@
+// Link: https://www.codewars.com/kata/51f9d93b4095e0a7200001b8/train/javascript
+
+// SOLUTION ///////////////////////////////////////////////////////////////////
+
+function howManyLightsabersDoYouOwn(name) {
+  return name === 'Zach' ? 18 : 0;
+}
