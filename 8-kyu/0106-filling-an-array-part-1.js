0cb8538395d60e4a177e2002edf194e3641dad9b
Tomasz Bielski <ttbielski@gmail.com>
2022-10-11 17:53:46 +0200

    Add solution

---
+++ 8-kyu/0106-filling-an-array-part-1.js
@@ -0,0 +1,13 @@
+// Link: https://www.codewars.com/kata/571d42206414b103dc0006a1/train/javascript
+
+// SOLUTION ///////////////////////////////////////////////////////////////////
+
+const arr = N => {
+  const numbers = [];
+  
+  for (let i = 0; i < N; i++) {
+    numbers.push(i);
+  }
+  
+  return numbers;
+};
