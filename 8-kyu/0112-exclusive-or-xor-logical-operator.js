1f3c14f557056a13216ac7ce00c8ba08c2f3b5e0
Tomasz Bielski <ttbielski@gmail.com>
2022-10-13 17:03:01 +0200

    Add solution

---
+++ 8-kyu/0112-exclusive-or-xor-logical-operator.js
@@ -0,0 +1,7 @@
+// Link: https://www.codewars.com/kata/56fa3c5ce4d45d2a52001b3c/train/javascript
+
+// SOLUTION ///////////////////////////////////////////////////////////////////
+
+function xor(a, b) {
+  return a !== b;
+}
