327bb83e686c06d60fa0e2dca301772c8b60ee2a
Tomasz Bielski <ttbielski@gmail.com>
2022-10-13 17:07:39 +0200

    Add solution

---
+++ 8-kyu/0113-create-your-first-js-function-and-print-hello-world.js
@@ -0,0 +1,8 @@
+// Link: https://www.codewars.com/kata/571ec274b1c8d4a61c0000c8/train/javascript
+
+// SOLUTION ///////////////////////////////////////////////////////////////////
+
+function helloWorld() {
+  let str = 'Hello World!';
+  console.log(str);
+}
