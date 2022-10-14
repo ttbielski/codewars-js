b974703e35830d85145de205c62e3fa2530ad901
Tomasz Bielski <ttbielski@gmail.com>
2022-10-14 17:23:49 +0200

    Add solution

---
+++ 8-kyu/0115-get-character-from-ascii-value.js
@@ -0,0 +1,7 @@
+// Link: https://www.codewars.com/kata/55ad04714f0b468e8200001c/train/javascript
+
+// SOLUTION ///////////////////////////////////////////////////////////////////
+
+function getChar(c){
+  return String.fromCharCode(c);
+}
