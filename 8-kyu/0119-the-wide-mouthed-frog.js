489f20f6e59188f7d8344f40090dd029785fa8f2
Tomasz Bielski <ttbielski@gmail.com>
2022-10-17 19:14:48 +0200

    Add solution

---
+++ 8-kyu/0119-the-wide-mouthed-frog.js
@@ -0,0 +1,7 @@
+// Link: https://www.codewars.com/kata/57ec8bd8f670e9a47a000f89/train/javascript
+
+// SOLUTION ///////////////////////////////////////////////////////////////////
+
+function mouthSize(animal) {
+  return animal.toLowerCase() === 'alligator' ? 'small' : 'wide';
+}
