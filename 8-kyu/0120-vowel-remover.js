5f5a4050e77e0109677eb57934aba2ac1bb67b93
Tomasz Bielski <ttbielski@gmail.com>
2022-10-17 19:27:44 +0200

    Add solution

---
+++ 8-kyu/0120-vowel-remover.js
@@ -0,0 +1,7 @@
+// Link: https://www.codewars.com/kata/5547929140907378f9000039/train/javascript
+
+// SOLUTION ///////////////////////////////////////////////////////////////////
+
+function shortcut(string) {
+  return string.replace(/[aeiou]/g, '');
+}
