d7d4fb916ef418aafe66101f9cee0cf560f30eb9
Tomasz Bielski <ttbielski@gmail.com>
2022-10-18 23:22:17 +0200

    Add solution

---
+++ 8-kyu/0122-convert-to-binary.js
@@ -0,0 +1,7 @@
+// Link: https://www.codewars.com/kata/59fca81a5712f9fa4700159a/train/javascript
+
+// SOLUTION ///////////////////////////////////////////////////////////////////
+
+function toBinary(n){
+  return Number(n.toString(2));
+}
