76611d4bb280126fd08fbc94c8cdfdc5c42520fa
Tomasz Bielski <ttbielski@gmail.com>
2022-10-19 20:52:18 +0200

    Add solution

---
+++ 8-kyu/0124-holiday-viii-duty-free.js
@@ -0,0 +1,7 @@
+// Link: https://www.codewars.com/kata/57e92e91b63b6cbac20001e5/train/javascript
+
+// SOLUTION ///////////////////////////////////////////////////////////////////
+
+function dutyFree(normPrice, discount, hol){
+  return Math.floor(hol / normPrice / discount * 100);
+}
