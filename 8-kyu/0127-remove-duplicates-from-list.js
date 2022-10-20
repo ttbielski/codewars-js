60a9b89730d0df9cf860f821cb16316cd894b851
Tomasz Bielski <ttbielski@gmail.com>
2022-10-20 16:22:22 +0200

    Add solution

---
+++ 8-kyu/0127-remove-duplicates-from-list.js
@@ -0,0 +1,7 @@
+// Link: https://www.codewars.com/kata/57a5b0dfcf1fa526bb000118/train/javascript
+
+// SOLUTION ///////////////////////////////////////////////////////////////////
+
+function distinct(a) {
+  return [...new Set(a)];
+}
