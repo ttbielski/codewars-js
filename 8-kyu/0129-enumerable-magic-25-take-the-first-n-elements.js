0941596c597e7c455d012797f48289f70b21afe3
Tomasz Bielski <ttbielski@gmail.com>
2022-10-20 16:30:17 +0200

    Add solution

---
+++ 8-kyu/0129-enumerable-magic-25-take-the-first-n-elements.js
@@ -0,0 +1,7 @@
+// Link: https://www.codewars.com/kata/545afd0761aa4c3055001386/train/javascript
+
+// SOLUTION ///////////////////////////////////////////////////////////////////
+
+function take(arr, n) {
+  return arr.slice(0, n);
+}
