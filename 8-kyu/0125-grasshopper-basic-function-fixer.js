639aee5088509e4185ab330fb45ecc4019247b88
Tomasz Bielski <ttbielski@gmail.com>
2022-10-20 16:11:40 +0200

    Add solution

---
+++ 8-kyu/0125-grasshopper-basic-function-fixer.js
@@ -0,0 +1,7 @@
+// Link: https://www.codewars.com/kata/56200d610758762fb0000002/train/javascript
+
+// SOLUTION ///////////////////////////////////////////////////////////////////
+
+function addFive(num) {
+  return num + 5;
+}
