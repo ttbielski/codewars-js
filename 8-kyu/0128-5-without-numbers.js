0c66d58058cb6944a44b52202b511ff13641aa85
Tomasz Bielski <ttbielski@gmail.com>
2022-10-20 16:24:34 +0200

    Add solution

---
+++ 8-kyu/0128-5-without-numbers.js
@@ -0,0 +1,7 @@
+// Link: https://www.codewars.com/kata/59441520102eaa25260000bf/train/javascript
+
+// SOLUTION ///////////////////////////////////////////////////////////////////
+
+function unusualFive() {
+  return 'hello'.length;
+}
