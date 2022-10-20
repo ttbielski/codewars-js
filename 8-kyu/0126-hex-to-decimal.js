dbc22bd7d9144a52e29973bb9da5e6e54337d50a
Tomasz Bielski <ttbielski@gmail.com>
2022-10-20 16:17:58 +0200

    Add solution

---
+++ 8-kyu/0126-hex-to-decimal.js
@@ -0,0 +1,7 @@
+// Link: https://www.codewars.com/kata/56200d610758762fb0000002/train/javascript
+
+// SOLUTION ///////////////////////////////////////////////////////////////////
+
+function hexToDec(hexString){
+  return parseInt(hexString, 16);
+}
