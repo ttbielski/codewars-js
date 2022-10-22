3d43e53bde6d0fe32f1a682c2093fd93bf6d31ed
Tomasz Bielski <ttbielski@gmail.com>
2022-10-22 17:28:35 +0200

    Add solution

---
+++ 8-kyu/0132-grasshopper-function-syntax-debugging.js
@@ -0,0 +1,7 @@
+// Link: https://www.codewars.com/kata/56dae9dc54c0acd29d00109a/train/javascript
+
+// SOLUTION ///////////////////////////////////////////////////////////////////
+
+function main(verb, noun) {
+  return verb + noun;
+}
