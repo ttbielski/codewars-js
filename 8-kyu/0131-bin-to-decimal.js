879bbbb45a648d631aec8e510f3a6beb529340cb
Tomasz Bielski <ttbielski@gmail.com>
2022-10-22 17:26:24 +0200

    Add solution

---
+++ 8-kyu/0131-bin-to-decimal.js
@@ -0,0 +1,7 @@
+// Link: https://www.codewars.com/kata/57a5c31ce298a7e6b7000334/train/javascript
+
+// SOLUTION ///////////////////////////////////////////////////////////////////
+
+function binToDec(bin){
+  return parseInt(bin, 2);
+}
