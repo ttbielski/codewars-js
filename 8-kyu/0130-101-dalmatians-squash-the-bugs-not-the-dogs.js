892dbe26346d15973f37d2eb195c06b837c8be12
Tomasz Bielski <ttbielski@gmail.com>
2022-10-22 17:21:30 +0200

    Add solution

---
+++ 8-kyu/0130-101-dalmatians-squash-the-bugs-not-the-dogs.js
@@ -0,0 +1,9 @@
+// Link: https://www.codewars.com/kata/56f6919a6b88de18ff000b36/train/javascript
+
+// SOLUTION ///////////////////////////////////////////////////////////////////
+
+function howManyDalmatians(number) {
+  var dogs = ["Hardly any", "More than a handful!", "Woah that's a lot of dogs!", "101 DALMATIANS!!!"];
+  
+  return respond = number <= 10 ? dogs[0] : number <= 50 ? dogs[1] : number === 101 ? dogs[3] : dogs[2];
+}
