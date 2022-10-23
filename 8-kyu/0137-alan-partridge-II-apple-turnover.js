91770e0d1667a0814b8cf83b50360f2906bc9d9d
Tomasz Bielski <ttbielski@gmail.com>
2022-10-23 17:39:34 +0200

    Add solution

---
+++ 8-kyu/0137-alan-partridge-II-apple-turnover.js
@@ -0,0 +1,7 @@
+// Link: https://www.codewars.com/kata/580a094553bd9ec5d800007d/train/javascript
+
+// SOLUTION ///////////////////////////////////////////////////////////////////
+
+function apple(x) {
+  return x ** 2 > 1000 ? `It's hotter than the sun!!` : `Help yourself to a honeycomb Yorkie for the glovebox.`;
+}
