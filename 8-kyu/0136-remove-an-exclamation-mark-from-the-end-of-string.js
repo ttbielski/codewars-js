51a84e861056f273376be2384578db9a969f5193
Tomasz Bielski <ttbielski@gmail.com>
2022-10-23 17:35:23 +0200

    Add solution

---
+++ 8-kyu/0136-remove-an-exclamation-mark-from-the-end-of-string.js
@@ -0,0 +1,7 @@
+// Link: https://www.codewars.com/kata/57fae964d80daa229d000126/train/javascript
+
+// SOLUTION ///////////////////////////////////////////////////////////////////
+
+function remove(string) {
+  return string.replace(/!$/, '');
+}
