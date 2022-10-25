9ef81c09c8933087b4e870039b8fabeab5cb0ddc
Tomasz Bielski <ttbielski@gmail.com>
2022-10-25 23:32:30 +0200

    Add solution

---
+++ 8-kyu/0139-super-duper-easy.js
@@ -0,0 +1,7 @@
+// Link: https://www.codewars.com/kata/55a5bfaa756cfede78000026/train/javascript
+
+// SOLUTION ///////////////////////////////////////////////////////////////////
+
+function problem(x) {
+  return (typeof x === 'string') ? 'Error' : x * 50 + 6;
+}
