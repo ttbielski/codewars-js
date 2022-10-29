d2ad62a53618b721c1dfd0dd19f1d5186280b063
Tomasz Bielski <ttbielski@gmail.com>
2022-10-29 20:11:11 +0200

    Add solution

---
+++ 8-kyu/0142-training-js-2-basic-data-types-number.js
@@ -0,0 +1,35 @@
+// Link: https://www.codewars.com/kata/571edd157e8954bab500032d/train/javascript
+
+// SOLUTION ///////////////////////////////////////////////////////////////////
+
+let v1 = 50,
+    v2 = 100,
+    v3 = 150,
+    v4 = 200,
+    v5 = 2,
+    v6 = 250;
+
+function equal1(){
+  let a = v1, b = v1;   
+  return a + b;
+}
+
+function equal2(){
+  let a = v4, b = v2;
+  return a - b;
+}
+
+function equal3(){
+  let a = v1, b = v5;
+  return a * b;
+}
+
+function equal4(){
+  let a = v4, b = v5;
+  return a / b;
+}
+
+function equal5(){
+  let a = v2, b = v3;
+  return a % b;
+}
