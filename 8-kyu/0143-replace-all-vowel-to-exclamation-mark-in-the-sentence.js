fa4bac628b8a68c0f64c4ee668537f3349ee90aa
Tomasz Bielski <ttbielski@gmail.com>
2022-10-29 20:14:56 +0200

    Add solution

---
+++ 8-kyu/0143-replace-all-vowel-to-exclamation-mark-in-the-sentence.js
@@ -0,0 +1,7 @@
+// Link: https://www.codewars.com/kata/57fb09ef2b5314a8a90001ed/train/javascript
+
+// SOLUTION ///////////////////////////////////////////////////////////////////
+
+function replace(s) {
+  return s.replace(/[aeiou]/gi, '!');
+}
