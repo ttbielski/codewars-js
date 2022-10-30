e53462c95fce9addaa213a3256cb8c47079bcd84
Tomasz Bielski <ttbielski@gmail.com>
2022-10-30 20:32:54 +0100

    Add solution

---
+++ 7-kyu/0002-disemvowel-trolls.js
@@ -0,0 +1,7 @@
+// Link: https://www.codewars.com/kata/52fba66badcd10859f00097e/train/javascript
+
+// SOLUTION ///////////////////////////////////////////////////////////////////
+
+function disemvowel(str) {
+  return str.replace(/[aeiou]/gi, '');
+}
