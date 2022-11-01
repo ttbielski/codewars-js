c92fd0b25687bf6512e0b0e75c056ba37113fa59
Tomasz Bielski <ttbielski@gmail.com>
2022-11-01 17:31:30 +0100

    Add solution

---
+++ 7-kyu/0008-list-filtering.js
@@ -0,0 +1,7 @@
+// Link: https://www.codewars.com/kata/53dbd5315a3c69eed20002dd/train/javascript
+
+// SOLUTION ///////////////////////////////////////////////////////////////////
+
+function filter_list(l) {
+  return l.filter(num => typeof num === 'number');
+}
