fac985ce29f9f4c4615ceea10b0a5f91114fa8cb
Tomasz Bielski <ttbielski@gmail.com>
2022-11-01 17:24:18 +0100

    Add solution

---
+++ 7-kyu/0006-you-are-a-square.js
@@ -0,0 +1,7 @@
+// Link: https://www.codewars.com/kata/54c27a33fb7da0db0100040e/train/javascript
+
+// SOLUTION ///////////////////////////////////////////////////////////////////
+
+function isSquare(n) {
+  return Number.isInteger(Math.sqrt(n));
+}
