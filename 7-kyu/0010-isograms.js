8e4b1efc0d7e9a2da37216402ec0467565d752f8
Tomasz Bielski <ttbielski@gmail.com>
2022-11-02 01:58:06 +0100

    Add solution

---
+++ 7-kyu/0010-isograms.js
@@ -0,0 +1,7 @@
+// Link: https://www.codewars.com/kata/54ba84be607a92aa900000f1/train/javascript
+
+// SOLUTION ///////////////////////////////////////////////////////////////////
+
+function isIsogram(str){
+  return new Set(str.toLowerCase()).size === str.length;
+}
