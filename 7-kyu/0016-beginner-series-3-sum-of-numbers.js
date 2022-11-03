600f5fdc8d2d3c59b7258202ede0f3d8219c9674
Tomasz Bielski <ttbielski@gmail.com>
2022-11-03 01:10:33 +0100

    Add solution

---
+++ 7-kyu/0016-beginner-series-3-sum-of-numbers.js
@@ -0,0 +1,20 @@
+// Link: https://www.codewars.com/kata/55f2b110f61eb01779000053/train/javascript
+
+// SOLUTION ///////////////////////////////////////////////////////////////////
+
+function getSum(a, b) {
+  
+  if (a > b) [a, b] = [b, a];
+  
+  if (a === b) {
+    return a;
+  } else {
+    let sum = 0;
+    
+    for (let i = a; i <= b; i++) {
+      sum += i;
+    }
+    
+    return sum;
+  }
+}
