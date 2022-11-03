aad1979f96c87a1d6b9554ffc8a35f29dfb8f36c
Tomasz Bielski <ttbielski@gmail.com>
2022-11-03 01:29:28 +0100

    Add solution

---
+++ 7-kyu/0020-string-ends-with.js
@@ -0,0 +1,7 @@
+// Link: https://www.codewars.com/kata/51f2d1cafc9c0f745c00037d/train/javascript
+
+// SOLUTION ///////////////////////////////////////////////////////////////////
+
+function solution(str, ending){
+  return str.endsWith(ending);
+}
