bdc1d56004325adeb19a070410450fbff0fa03a4
Tomasz Bielski <ttbielski@gmail.com>
2022-11-03 01:21:08 +0100

    Add solution

---
+++ 7-kyu/0017-friend-or-foe.js
@@ -0,0 +1,7 @@
+// Link: https://www.codewars.com/kata/55b42574ff091733d900002f/train/javascript
+
+// SOLUTION ///////////////////////////////////////////////////////////////////
+
+function friend(friends) {
+  return friends.filter(name => name.length === 4);
+}
