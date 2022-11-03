7e40c15d57ec2ba1430ca22b675c44de728434ed
Tomasz Bielski <ttbielski@gmail.com>
2022-11-03 01:42:24 +0100

    Add solution

---
+++ 7-kyu/0023-regex-validate-pin-code.js
@@ -0,0 +1,7 @@
+// Link: https://www.codewars.com/kata/55f8a9c06c018a0d6e000132/train/javascript
+
+// SOLUTION ///////////////////////////////////////////////////////////////////
+
+function validatePIN(pin) {
+  return (/^(\d{4}|\d{6})$/).test(pin);
+}
