93ec071e3d364cf37fe04ce33382a315a84b95f1
Tomasz Bielski <ttbielski@gmail.com>
2022-11-03 01:38:42 +0100

    Add solution

---
+++ 7-kyu/0022-binary-addition.js
@@ -0,0 +1,7 @@
+// Link: https://www.codewars.com/kata/551f37452ff852b7bd000139/train/javascript
+
+// SOLUTION ///////////////////////////////////////////////////////////////////
+
+function addBinary(a, b) {
+  return (a + b).toString(2);
+}
