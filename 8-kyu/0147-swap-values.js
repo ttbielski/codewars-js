37592c923ad5e1f1936f3a9eaa46595a95b8d81a
Tomasz Bielski <ttbielski@gmail.com>
2022-11-04 01:59:24 +0100

    Add solution

---
+++ 8-kyu/0147-swap-values.js
@@ -0,0 +1,7 @@
+// Link: https://www.codewars.com/kata/5388f0e00b24c5635e000fc6/train/javascript
+
+// SOLUTION ///////////////////////////////////////////////////////////////////
+
+function swapValues(args) {
+  return args.reverse();
+}
