a6da6e575e9b5f9c6dc2c2df4e398eec25449c6d
Tomasz Bielski <ttbielski@gmail.com>
2022-11-04 02:02:58 +0100

    Change solution

---
+++ 8-kyu/0148-printing-array-elements-with-comma-delimiters.js
@@ -0,0 +1,7 @@
+// Link: https://www.codewars.com/kata/56e2f59fb2ed128081001328/train/javascript
+
+// SOLUTION ///////////////////////////////////////////////////////////////////
+
+function printArray(array) {
+  return array.join();
+}
