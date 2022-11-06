a9039d077d0247e930c9bb060c97c77586deacfd
Tomasz Bielski <ttbielski@gmail.com>
2022-11-06 02:02:20 +0100

    Add solution

---
+++ 7-kyu/0024-sum-of-odd-numbers.js
@@ -0,0 +1,7 @@
+// Link: https://www.codewars.com/kata/55fd2d567d94ac3bc9000064/train/javascript
+
+// SOLUTION ///////////////////////////////////////////////////////////////////
+
+function rowSumOddNumbers(n) {
+  return n ** 3;
+}
