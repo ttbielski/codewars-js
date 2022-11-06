e9ed6b76699bd5fd11cbad51a2f1f7ff10d8cbdf
Tomasz Bielski <ttbielski@gmail.com>
2022-11-06 01:38:35 +0100

    Add solution

---
+++ 8-kyu/0150-pre-fizzbuzz-workout-1.js
@@ -0,0 +1,13 @@
+// Link: https://www.codewars.com/kata/569e09850a8e371ab200000b/train/javascript
+
+// SOLUTION ///////////////////////////////////////////////////////////////////
+
+function preFizz(n) {
+  const result = [];
+  
+  for (let i = 1; i <= n; i++) {
+    result.push(i);
+  }
+  
+  return result;
+}
