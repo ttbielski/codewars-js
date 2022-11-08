e14ed82a829585ab97f6b03805a5ea9070919176
Tomasz Bielski <ttbielski@gmail.com>
2022-11-08 01:12:10 +0100

    Add solution

---
+++ 8-kyu/0151-grasshopper-variable-assignment-debug.js
@@ -0,0 +1,8 @@
+// Link: https://www.codewars.com/kata/5612e743cab69fec6d000077/train/javascript
+
+// SOLUTION ///////////////////////////////////////////////////////////////////
+
+const a = "dev";
+const b = "Lab";
+
+const name = a + b;
