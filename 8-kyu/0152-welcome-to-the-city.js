e43d1591515ccd778741f88799efaf88013e1b2a
Tomasz Bielski <ttbielski@gmail.com>
2022-11-08 01:20:30 +0100

    Add solution

---
+++ 8-kyu/0152-welcome-to-the-city.js
@@ -0,0 +1,7 @@
+// Link: https://www.codewars.com/kata/5302d846be2a9189af0001e4/train/javascript
+
+// SOLUTION ///////////////////////////////////////////////////////////////////
+
+function sayHello(name, city, state) {
+  return `Hello, ${name.join(' ')}! Welcome to ${city}, ${state}!`;
+}
