2bf89f9102c0e57642581a97f2707364d3565341
Tomasz Bielski <ttbielski@gmail.com>
2022-11-08 23:26:30 +0100

    Add solution

---
+++ 8-kyu/0153-basic-training-add-item-to-an-array.js
@@ -0,0 +1,6 @@
+// Link: https://www.codewars.com/kata/511f0fe64ae8683297000001/train/javascript
+
+// SOLUTION ///////////////////////////////////////////////////////////////////
+
+// add the value "codewars" to the already defined websites array
+websites.push('codewars');
