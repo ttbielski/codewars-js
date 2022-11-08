df3fcea5f1fb40d1389863fbe6e69aab90cf4ec0
Tomasz Bielski <ttbielski@gmail.com>
2022-11-08 23:41:25 +0100

    Add solution

---
+++ 8-kyu/0154-reversing-words-in-a-string.js
@@ -0,0 +1,7 @@
+// Link: https://www.codewars.com/kata/57a55c8b72292d057b000594/train/javascript
+
+// SOLUTION ///////////////////////////////////////////////////////////////////
+
+function reverse(string) {
+  return string.split(' ').reverse().join(' ');
+}
