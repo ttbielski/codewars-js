f1bd1c459ea50c3e66324c56341cd16310779c73
Tomasz Bielski <ttbielski@gmail.com>
2022-11-08 23:53:52 +0100

    Add solution

---
+++ 8-kyu/0155-remove-first-and-last-character-part-two.js
@@ -0,0 +1,7 @@
+// Link: https://www.codewars.com/kata/570597e258b58f6edc00230d/train/javascript
+
+// SOLUTION ///////////////////////////////////////////////////////////////////
+
+function array(arr) {
+  return arr.split(',').slice(1, -1).join(' ') || null;
+}
