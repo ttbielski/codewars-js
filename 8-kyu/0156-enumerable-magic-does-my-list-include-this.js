f2b8d0047005e13b6cd4ee6c7d3b16d40d84d8ce
Tomasz Bielski <ttbielski@gmail.com>
2022-11-09 01:09:20 +0100

    Add solution

---
+++ 8-kyu/0156-enumerable-magic-does-my-list-include-this.js
@@ -0,0 +1,7 @@
+// Link: https://www.codewars.com/kata/545991b4cbae2a5fda000158/train/javascript
+
+// SOLUTION ///////////////////////////////////////////////////////////////////
+
+function include(arr, item) {
+  return arr.includes(item);
+}
