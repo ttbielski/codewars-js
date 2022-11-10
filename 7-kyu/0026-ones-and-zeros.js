97eee0ad3843c2d2db09c979adc0c0f8a86a5cba
Tomasz Bielski <ttbielski@gmail.com>
2022-11-10 19:40:15 +0100

    Add solution

---
+++ 7-kyu/0026-ones-and-zeros.js
@@ -0,0 +1,5 @@
+// Link: https://www.codewars.com/kata/578553c3a1b8d5c40300037c/train/javascript
+
+// SOLUTION ///////////////////////////////////////////////////////////////////
+
+const binaryArrayToNumber = (arr) => parseInt(arr.join(''), 2);
