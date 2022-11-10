60b37554f2f6fd5a52e35ec4225aff143ea8d8f9
Tomasz Bielski <ttbielski@gmail.com>
2022-11-10 01:31:01 +0100

    Add solution

---
+++ 8-kyu/0157-usd-cny.js
@@ -0,0 +1,7 @@
+// Link: https://www.codewars.com/kata/5977618080ef220766000022/train/javascript
+
+// SOLUTION ///////////////////////////////////////////////////////////////////
+
+function usdcny(usd) {
+  return `${(usd * 6.75).toFixed(2)} Chinese Yuan`;
+}
