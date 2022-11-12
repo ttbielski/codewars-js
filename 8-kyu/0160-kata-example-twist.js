9a5d797004533bf1b54a982ef387dc868eb7afc8
Tomasz Bielski <ttbielski@gmail.com>
2022-11-13 00:24:24 +0100

    Add solution

---
+++ 8-kyu/0160-kata-example-twist.js
@@ -0,0 +1,5 @@
+// Link: https://www.codewars.com/kata/525c1a07bb6dda6944000031/train/javascript
+
+// SOLUTION ///////////////////////////////////////////////////////////////////
+
+const websites = new Array(1000).fill('codewars');
