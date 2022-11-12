5ae0889d3ffe43f4afbfb354a12148d1ca3161ac
Tomasz Bielski <ttbielski@gmail.com>
2022-11-13 00:34:47 +0100

    Add solution

---
+++ 8-kyu/0161-find-the-position.js
@@ -0,0 +1,9 @@
+// Link: https://www.codewars.com/kata/5808e2006b65bff35500008f/train/javascript
+
+// SOLUTION ///////////////////////////////////////////////////////////////////
+
+function position(letter) {
+  const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+  
+  return `Position of alphabet: ${alphabet.indexOf(letter) + 1}`;
+}
