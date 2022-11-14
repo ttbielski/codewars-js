e6ccc04dabf05a84680a88d62a266134c2f73201
Tomasz Bielski <ttbielski@gmail.com>
2022-11-14 22:27:28 +0100

    Add solution

---
+++ 8-kyu/0163-grasshopper-array-mean.js
@@ -0,0 +1,5 @@
+// Link: https://www.codewars.com/kata/55d277882e139d0b6000005d/train/javascript
+
+// SOLUTION ///////////////////////////////////////////////////////////////////
+
+const findAverage = (nums) => nums.reduce((a, b) => a + b, 0) / nums.length;
