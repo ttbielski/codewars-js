df8f93fe89629b2e0eafbfa0aee2404feac8a36e
Tomasz Bielski <ttbielski@gmail.com>
2022-11-16 09:40:48 +0100

    Add solution

---
+++ 7-kyu/0030-sort-numbers.js
@@ -0,0 +1,7 @@
+// Link: https://www.codewars.com/kata/5174a4c0f2769dd8b1000003/train/javascript
+
+// SOLUTION ///////////////////////////////////////////////////////////////////
+
+function solution(nums) {
+  return (nums || []).sort((a, b) => a - b);
+}
