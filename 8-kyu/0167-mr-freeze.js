b8a426068915aec530176e01cf717ff5690f1efb
Tomasz Bielski <ttbielski@gmail.com>
2022-11-20 02:06:15 +0100

    Add solution

---
+++ 8-kyu/0167-mr-freeze.js
@@ -0,0 +1,5 @@
+// Link: https://www.codewars.com/kata/514a3996d22ce03198000003/train/javascript
+
+// SOLUTION ///////////////////////////////////////////////////////////////////
+
+Object.freeze(MrFreeze);
