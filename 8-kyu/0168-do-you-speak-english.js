a67e4ad2d300b417ae0371d8216d75212227de7f
Tomasz Bielski <ttbielski@gmail.com>
2022-11-20 02:12:13 +0100

    Add solution

---
+++ 8-kyu/0168-do-you-speak-english.js
@@ -0,0 +1,7 @@
+// Link: https://www.codewars.com/kata/58dbdccee5ee8fa2f9000058/train/javascript
+
+// SOLUTION ///////////////////////////////////////////////////////////////////
+
+function spEng(sentence) {
+  return /english/i.test(sentence);
+}
