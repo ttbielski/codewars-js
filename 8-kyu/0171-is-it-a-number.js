b596348446b5bf59e1433edcc15fea4595685688
Tomasz Bielski <ttbielski@gmail.com>
2022-11-26 02:18:37 +0100

    Add solution

---
+++ 8-kyu/0171-is-it-a-number.js
@@ -0,0 +1,7 @@
+// Link: https://www.codewars.com/kata/57126304cdbf63c6770012bd/train/javascript
+
+// SOLUTION ///////////////////////////////////////////////////////////////////
+
+function isDigit(s) {
+  return parseFloat(s) === Number(s);
+}
