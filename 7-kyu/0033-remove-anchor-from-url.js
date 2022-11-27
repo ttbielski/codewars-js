1f617ef9f2b2f8e46dccdbf788aa4d4b44eac2eb
Tomasz Bielski <ttbielski@gmail.com>
2022-11-27 05:32:17 +0100

    Add solution

---
+++ 7-kyu/0033-remove-anchor-from-url.js
@@ -0,0 +1,7 @@
+// Link: https://www.codewars.com/kata/51f2b4448cadf20ed0000386/train/javascript
+
+// SOLUTION ///////////////////////////////////////////////////////////////////
+
+function removeUrlAnchor(url) {
+  return url.replace(/#.*/, '');
+}
