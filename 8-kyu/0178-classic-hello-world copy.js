baa6ccaa009e09503effa46e02fe5b3e61c96804
Tomasz Bielski <ttbielski@gmail.com>
2022-12-02 22:32:44 +0100

    Add solution

---
+++ 8-kyu/0178-classic-hello-world copy.js
@@ -0,0 +1,7 @@
+// Link: https://www.codewars.com/kata/5a2e9ae2b6cfd7692a0000ba/train/javascript
+
+// SOLUTION ///////////////////////////////////////////////////////////////////
+
+function typeOfSum(a, b) {
+  return typeof (a + b);
+}
