fa9f05bd8a276dae9d9933e957f7f8993bb29f81
Tomasz Bielski <ttbielski@gmail.com>
2022-12-02 22:30:11 +0100

    Add solution

---
+++ 8-kyu/0177-classic-hello-world.js
@@ -0,0 +1,9 @@
+// Link: https://www.codewars.com/kata/57036f007fd72e3b77000023/train/javascript
+
+// SOLUTION ///////////////////////////////////////////////////////////////////
+
+class Solution {
+  static main() {
+    console.log('Hello World!');
+  }
+}
