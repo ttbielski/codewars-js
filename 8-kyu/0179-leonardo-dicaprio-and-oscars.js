b5d89adde5acbe3619a92911cf9895cb23bd9887
Tomasz Bielski <ttbielski@gmail.com>
2022-12-02 22:39:51 +0100

    Add solution

---
+++ 8-kyu/0179-leonardo-dicaprio-and-oscars.js
@@ -0,0 +1,15 @@
+// Link: https://www.codewars.com/kata/56d49587df52101de70011e4/train/javascript
+
+// SOLUTION ///////////////////////////////////////////////////////////////////
+
+function leo(oscar) {
+  if (oscar > 88) {
+    return 'Leo got one already!';
+  } else if (oscar === 88) {
+    return 'Leo finally won the oscar! Leo is happy';
+  } else if (oscar === 86) {
+    return 'Not even for Wolf of wallstreet?!';
+  } else {
+    return 'When will you give Leo an Oscar?';
+  }
+}
