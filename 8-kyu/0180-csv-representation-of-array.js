81bdcaceb71bd6c6a7ce81b010a1ea54c24a9817
Tomasz Bielski <ttbielski@gmail.com>
2022-12-06 10:48:52 +0100

    Add solution

---
+++ 8-kyu/0180-csv-representation-of-array.js
@@ -0,0 +1,7 @@
+// Link: https://www.codewars.com/kata/5a34af40e1ce0eb1f5000036/train/javascript
+
+// SOLUTION ///////////////////////////////////////////////////////////////////
+
+function toCsvText(array) {
+  return array.join('\n');
+}
