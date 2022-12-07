6fd92746effc1ec9a2007a6aea5ece13176d5dfa
Tomasz Bielski <ttbielski@gmail.com>
2022-12-07 15:27:01 +0100

    Add solution

---
+++ 8-kyu/0181-are-arrow-functions-odd.js
@@ -0,0 +1,7 @@
+// Link: https://www.codewars.com/kata/559f80b87fa8512e3e0000f5/train/javascript
+
+// SOLUTION ///////////////////////////////////////////////////////////////////
+
+function odds(values) {
+  return values.filter(number => number % 2 !== 0);
+}
