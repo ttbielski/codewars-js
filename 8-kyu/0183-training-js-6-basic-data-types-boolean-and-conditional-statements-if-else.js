e0e708af039cddd21572b708a98072d716d25322
Tomasz Bielski <ttbielski@gmail.com>
2022-12-09 09:52:50 +0100

    Add solution

---
+++ 8-kyu/0183-training-js-6-basic-data-types-boolean-and-conditional-statements-if-else.js
@@ -0,0 +1,11 @@
+// Link: https://www.codewars.com/kata/571f832f07363d295d001ba8/train/javascript
+
+// SOLUTION ///////////////////////////////////////////////////////////////////
+
+function trueOrFalse(val) {
+  if (val) {
+    return 'true';
+  } else {
+    return 'false';
+  }
+}
