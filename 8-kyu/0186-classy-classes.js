92b89c91cb39650d0b0e6225681614c8af56c065
Tomasz Bielski <ttbielski@gmail.com>
2022-12-19 22:40:40 +0100

    Add solution

---
+++ 8-kyu/0186-classy-classes.js
@@ -0,0 +1,14 @@
+// Link: https://www.codewars.com/kata/55a144eff5124e546400005a/train/javascript
+
+// SOLUTION ///////////////////////////////////////////////////////////////////
+
+class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+  
+  get info() {
+    return `${this.name}s age is ${this.age}`;
+  }
+}
