158de4f8aadcd29903c141709f58f8132f4bfe02
Tomasz Bielski <ttbielski@gmail.com>
2022-12-20 00:48:36 +0100

    Add solution

---
+++ 8-kyu/0187-remove-the-time.js
@@ -0,0 +1,7 @@
+// Link: https://www.codewars.com/kata/56b0ff16d4aa33e5bb00008e/train/javascript
+
+// SOLUTION ///////////////////////////////////////////////////////////////////
+
+function shortenToDate(longDate) {
+  return longDate.replace(/,.*/, '');
+}
