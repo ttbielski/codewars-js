b61eb9f63907d4fc2def177b94b0c864c6b1a08d
Tomasz Bielski <ttbielski@gmail.com>
2022-12-27 22:36:15 +0100

    Add solution

---
+++ 8-kyu/0190-validate-code-with-simple-regex.js
@@ -0,0 +1,7 @@
+// Link: https://www.codewars.com/kata/56a25ba95df27b7743000016/train/javascript
+
+// SOLUTION ///////////////////////////////////////////////////////////////////
+
+function validateCode(code) {
+  return /^[123]/.test(code);
+}
