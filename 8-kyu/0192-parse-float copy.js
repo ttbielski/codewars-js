1720e35c9392190b007bb474f9da69a1bee2addd
Tomasz Bielski <ttbielski@gmail.com>
2023-01-03 22:32:17 +0100

    Add solution

---
+++ 8-kyu/0192-parse-float copy.js
@@ -0,0 +1,7 @@
+// Link: https://www.codewars.com/kata/55acfc59c3c23d230f00006d/train/javascript
+
+// SOLUTION ///////////////////////////////////////////////////////////////////
+
+function getASCII(c) {
+  return c.charCodeAt(0);
+}
