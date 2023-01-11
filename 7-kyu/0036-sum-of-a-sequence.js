e6a63072878ddedb0db5bf4af69cc23f98e99251
Tomasz Bielski <ttbielski@gmail.com>
2023-01-11 23:44:58 +0100

    Add solution

---
+++ 7-kyu/0036-sum-of-a-sequence.js
@@ -0,0 +1,13 @@
+// Link: https://www.codewars.com/kata/586f6741c66d18c22800010a/train/javascript
+
+// SOLUTION ///////////////////////////////////////////////////////////////////
+
+const sequenceSum = (begin, end, step) => {
+  let result = 0;
+  
+  for (let i = begin; i <= end; i += step) {
+    result += i;
+  }
+  
+  return result;
+};
