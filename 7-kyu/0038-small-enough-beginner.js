51b3bf72f2864c95017a19449deeee35c456c9fe
Tomasz Bielski <ttbielski@gmail.com>
2023-01-20 23:27:00 +0100

    Add solution

---
+++ 7-kyu/0038-small-enough-beginner.js
@@ -0,0 +1,7 @@
+// Link: https://www.codewars.com/kata/57cc981a58da9e302a000214/train/javascript
+
+// SOLUTION ///////////////////////////////////////////////////////////////////
+
+function smallEnough(a, limit) {
+  return Math.max(...a) <= limit;
+}
